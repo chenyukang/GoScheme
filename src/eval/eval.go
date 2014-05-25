22b458e77ca9dab1df0e2e4e02ca22c808bbc6f4
Yukang <moorekang@gmail.com>
2014-05-25 11:56:15 +0800

    continue base data structure

--- src/eval/eval.go
+++ src/eval/eval.go
@@ -1,143 +1,192 @@
 package eval
 
-type Object interface {}
-type Value  interface {}
-
 type ObjType int
+
 const (
-        EMPTY_LIST = iota
-        BOOLEAN
-        SYBOL
-        FIXNUM
-        CHARACTER
-        STRING
-        PAIR
-        PRIMITIVE_PROC
-        COMPOUND_PROC
-        INPUT_PORT
-        OUTPUT_PORT
-        EOF_OBJECT
+	UNDEF = iota
+	EMPTY_LIST
+	BOOLEAN
+	SYMBOL
+	FIXNUM
+	CHARACTER
+	STRING
+	PAIR
+	PRIMITIVE_PROC
+	COMPOUND_PROC
+	INPUT_PORT
+	OUTPUT_PORT
+	EOF_OBJECT
 )
 
-type EmptyList struct {
-        objType ObjType
+type ObjData struct {
+	boolean int
+	strVal  string
+	symbol  string
+	fixNum  int
+	char    byte
+
+	//pair data
+	car *Object
+	cdr *Object
 }
 
-type Boolean struct {
-        objType ObjType
-        value int
+type Object struct {
+	Type ObjType
+	Data ObjData
 }
 
-type Symbol struct {
-        objType ObjType
-        value string
+func allocObject() *Object {
+	obj := &Object{}
+	obj.Type = UNDEF
+	return obj
 }
 
-type FixNum struct {
-        objType ObjType
-        value   int64
-}
+var SymbolTable map[string]*Object
+var The_Empty_List *Object
+var The_True *Object
+var The_False *Object
+var Set_Symbol *Object
+var OK_Symbol *Object
+var If_Symbol *Object
+var Else_Symbol *Object
+var Cond_Symbol *Object
+var Or_Symbol *Object
+var Quote_Symbol *Object
+var Lambda_Symbol *Object
+var Define_Symbol *Object
+var Begin_Symbol *Object
+var Let_Symbol *Object
+var And_Symbol *Object
+var Eof_Symbol *Object
+var The_Empty_Env *Object
+var The_Global_Env *Object
 
-type Character struct {
-        objType ObjType
-        value   byte
-}
-
-type Pair struct {
-        objType ObjType
-        car     Object
-        cdr     Object
-}
-
-type Proc struct {
-        objType ObjType
-}
-
-var Symbol_Table   map[string]Object
-var The_Empty_List Object
-var The_True       Object
-var The_False      Object
-var Set_Symbol     Object
-var OK_Symbol      Object
-var If_Symbol      Object
-var Else_Symbol    Object
-var Cond_Symbol    Object
-var Or_Symbol      Object
-var Quote_Symbol   Object
-var Lambda_Symbol  Object
-var Define_Symbol  Object
-var Begin_Symbol   Object
-var Let_Symbol     Object
-var And_Symbol     Object
-var Eof_Symbol     Object
-var The_Empty_Env  Object
-var The_Global_Env Object
-
-func MakeSymbol(sym string) Object {
-        if res, ok := Symbol_Table[sym]; ok {
-                return res
-        }
-        obj := &Symbol{ STRING, sym}
-        Symbol_Table[sym] = (Object)(obj)
-        return obj
-}
-
-func IsBoolean(obj Object) bool {
-	if _, ok := obj.(*Boolean); ok {
-		return true
+func makeSymbol(sym string) *Object {
+	if obj, ok := SymbolTable[sym]; ok {
+		return obj
 	}
-	return false
+	obj := allocObject()
+	obj.Type = SYMBOL
+	obj.Data.symbol = sym
+	SymbolTable[sym] = obj
+	return obj
 }
 
-func IsFalse(obj Object) bool {
+func makeBoolean(val int) *Object {
+	obj := allocObject()
+	obj.Type = BOOLEAN
+	obj.Data.boolean = val
+	return obj
+}
+
+func isSymbol(obj *Object) bool {
+	return obj.Type == SYMBOL
+}
+
+func isBoolean(obj *Object) bool {
+	return obj.Type == BOOLEAN
+}
+
+func isFalse(obj *Object) bool {
 	return obj == The_False
 }
 
-func IsTrue(obj Object) bool {
-	return !IsFalse(obj)
+func isTrue(obj *Object) bool {
+	return !isFalse(obj)
 }
 
-func IsSymbol(obj Object) bool {
-	if _, ok := obj.(*Symbol); ok {
-		return true;
+func isEmptyList(obj *Object) bool {
+	return obj.Type == EMPTY_LIST
+}
+
+func isPair(obj *Object) bool {
+	return obj.Type == PAIR
+}
+
+func cons(car *Object, cdr *Object) *Object {
+	obj := allocObject()
+	obj.Type = PAIR
+	obj.Data.car = car
+	obj.Data.cdr = cdr
+	return obj
+}
+
+type Frame Object
+
+func makeFrame(vars *Object, vals *Object) *Object {
+	return cons(vars, vals)
+}
+
+func frameVars(frame *Object) *Object {
+	return frame.Data.car
+}
+
+func frameVals(frame *Object) *Object {
+	return frame.Data.cdr
+}
+
+func extendEnv(vars *Object, vals *Object, baseEnv *Object) *Object {
+	return cons(makeFrame(vars, vals), baseEnv)
+}
+
+func addBinding(avar *Object, aval *Object, frame *Object) {
+	frame.Data.car = cons(avar, frame.Data.car)
+	frame.Data.cdr = cons(aval, frame.Data.cdr)
+}
+
+func firstFrame(env *Object) *Object {
+	return env.Data.car
+}
+
+func lookupVar(avar *Object, env *Object) *Object {
+	e := env
+	for !isEmptyList(e) {
+		frame := firstFrame(env)
+		vars := frameVars(frame)
+		vals := frameVals(frame)
+		for !isEmptyList(vars) {
+			if avar == vars.Data.car {
+				return vals.Data.car
+			}
+			vars = vars.Data.cdr
+			vals = vals.Data.cdr
+		}
+		e = e.Data.cdr
 	}
-	return false
+	return nil
 }
 
-func IsEmptyList(obj Object) bool {
-	if _, ok := obj.(*EmptyList); ok {
-		return true
+func defineVar(avar *Object, aval *Object, env *Object) {
+	frame := firstFrame(env)
+	vars := frame.Data.car
+	vals := frame.Data.cdr
+	for !isEmptyList(vars) {
+		if avar == vars.Data.car {
+			vals.Data.cdr = aval
+			return
+		}
+		vars = vars.Data.cdr
+		vals = vals.Data.cdr
 	}
-	return false
-}
-
-func Cons(car Object, cdr Object) Object {
-        res := &Pair{PAIR, car, cdr}
-        return (Object)(res)
-}
-
-func IsPair(obj Object) bool {
-        if _, ok := obj.(*Pair); ok {
-                return true
-        }
-	return false
+	addBinding(avar, aval, env)
 }
 
 func Init() {
-        Symbol_Table = make(map[string]Object)
-        The_Empty_List = &EmptyList { EMPTY_LIST }
-	The_True = &Boolean{ BOOLEAN, 1}
-	The_False = &Boolean{ BOOLEAN, 0}
-        Set_Symbol = MakeSymbol("set!")
-        OK_Symbol = MakeSymbol("ok")
-        If_Symbol = MakeSymbol("if")
-	Else_Symbol = MakeSymbol("else")
-	Let_Symbol = MakeSymbol("let")
-	And_Symbol = MakeSymbol("and")
-	Or_Symbol = MakeSymbol("or")
-	Define_Symbol = MakeSymbol("define")
-        Begin_Symbol = MakeSymbol("begin")
-	Quote_Symbol = MakeSymbol("quote")
-	Lambda_Symbol = MakeSymbol("lambda")
-	Cond_Symbol = MakeSymbol("cond")
+	SymbolTable = make(map[string]*Object)
+	The_Empty_List = allocObject()
+	The_Empty_List.Type = EMPTY_LIST
+	The_True = makeBoolean(1)
+	The_False = makeBoolean(0)
+	Set_Symbol = makeSymbol("set!")
+	OK_Symbol = makeSymbol("ok")
+	If_Symbol = makeSymbol("if")
+	Else_Symbol = makeSymbol("else")
+	Let_Symbol = makeSymbol("let")
+	And_Symbol = makeSymbol("and")
+	Or_Symbol = makeSymbol("or")
+	Define_Symbol = makeSymbol("define")
+	Begin_Symbol = makeSymbol("begin")
+	Quote_Symbol = makeSymbol("quote")
+	Lambda_Symbol = makeSymbol("lambda")
+	Cond_Symbol = makeSymbol("cond")
 }
--- src/eval/eval_test.go
+++ src/eval/eval_test.go
@@ -1,23 +1,21 @@
 package eval
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestEmptyList(t *testing.T) {
 	Init()
-	if IsEmptyList(The_Empty_List) == false {
+	if isEmptyList(The_Empty_List) == false {
 		t.Error("empty_list failed")
 	}
 }
 
 func TestSymbol(t *testing.T) {
 	Init()
-	res := IsSymbol(OK_Symbol) && IsSymbol(Set_Symbol) && IsSymbol(If_Symbol)
-	res = res && IsSymbol(Begin_Symbol) && IsSymbol(Let_Symbol)
-	res = res && IsSymbol(And_Symbol) && IsSymbol(Or_Symbol)
-	res = res && IsSymbol(Lambda_Symbol)
-	res = res && IsSymbol(Quote_Symbol)
+	res := isSymbol(OK_Symbol) && isSymbol(Set_Symbol) && isSymbol(If_Symbol)
+	res = res && isSymbol(Begin_Symbol) && isSymbol(Let_Symbol)
+	res = res && isSymbol(And_Symbol) && isSymbol(Or_Symbol)
+	res = res && isSymbol(Lambda_Symbol)
+	res = res && isSymbol(Quote_Symbol)
 	if res == false {
 		t.Error("Symbol failed")
 	}
@@ -26,18 +24,18 @@ func TestSymbol(t *testing.T) {
 
 func TestTrueFalse(t *testing.T) {
 	Init()
-	if IsFalse(The_False) == false {
+	if isFalse(The_False) == false {
 		t.Error("False failed")
 	}
-	if IsTrue(The_True) == false {
+	if isTrue(The_True) == false {
 		t.Error("True failed")
 	}
 }
 
 func TestEverything(t *testing.T) {
 	Init()
-	res := Cons(OK_Symbol, If_Symbol)
-	if IsPair(res) == false {
+	res := cons(OK_Symbol, If_Symbol)
+	if isPair(res) == false {
 		t.Error("should be PAIR type")
 	}
 }
